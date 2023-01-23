09d075587bd1f08234b78901acf9a179d66d1bdf
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2023-01-23 14:34:37 +0100

    added GNL

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jsousa-a <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 12:01:49 by jsousa-a          #+#    #+#             */
-/*   Updated: 2022/12/02 11:35:52 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2023/01/23 14:31:17 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,5 +72,10 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-
+//****GNL****
+char	*g_free(char *str);
+char	*g_calloc(int ct);
+int		g_strlen(char *str);
+int		g_check_n(char *str);
+char	*get_next_line(int fd);
 #endif
